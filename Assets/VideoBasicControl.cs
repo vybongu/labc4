fa8b7da9d65334c0200e17ebb7f86616a6fd3b28
tule-bit <149056390+vybongu@users.noreply.github.com>
2026-01-30 00:57:37 +0700

    UPDATE

---
+++ Assets/VideoBasicControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoBasicControl : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+
+    void Start()
+    {
+        // Lấy component VideoPlayer gắn trên cùng Object
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        // Tắt tự động chạy khi vào game
+        videoPlayer.playOnAwake = false;
+    }
+
+    void Update()
+    {
+        // Nhấn V để Play video
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (!videoPlayer.isPlaying)
+            {
+                videoPlayer.Play();
+                Debug.Log("Video đang chạy!");
+            }
+        }
+    }
+}
---
+++ Assets/VideoEventManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoEventManager : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public GameObject endUI; // Kéo bảng UI thông báo vào đây
+
+    void Start()
+    {
+        // 1. Đăng ký sự kiện (Lắng nghe)
+        videoPlayer.prepareCompleted += OnVideoPrepared;
+        videoPlayer.loopPointReached += OnVideoFinished;
+
+        // Bắt đầu chuẩn bị video
+        videoPlayer.Prepare();
+        Debug.Log("Đang chuẩn bị video...");
+    }
+
+    // Hàm chạy khi video đã nạp xong
+    void OnVideoPrepared(VideoPlayer vp)
+    {
+        Debug.Log("Video đã sẵn sàng! Bắt đầu phát.");
+        vp.Play();
+    }
+
+    // Hàm chạy khi video phát hết
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        Debug.Log("Video đã kết thúc!");
+
+        // Lựa chọn 1: Hiện UI
+        if (endUI != null) endUI.SetActive(true);
+
+        // Lựa chọn 2: Chuyển sang Scene tiếp theo (Bỏ comment dòng dưới để dùng)
+        // SceneManager.LoadScene("NextLevelScene");
+    }
+}
